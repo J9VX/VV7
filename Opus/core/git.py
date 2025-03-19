41e896533a167de78503d4fe52924b56d6bf294a
J9VX <dhruvraj500@gmail.com>
2025-03-20 00:16:10 +0530

    Update git.py

--- Opus/core/git.py
+++ Opus/core/git.py
@@ -30,13 +30,13 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
 
 def git():
-    REPO_LINK = config.UPSTREAM_REPO
+    REPO_LINK = "https://github.com/J9VX/VX7"
     if config.GIT_TOKEN:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
         TEMP_REPO = REPO_LINK.split("https://")[1]
         UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
     else:
-        UPSTREAM_REPO = config.UPSTREAM_REPO
+        UPSTREAM_REPO = REPO_LINK
     try:
         repo = Repo()
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
@@ -58,7 +58,7 @@ def git():
         )
         repo.heads[config.UPSTREAM_BRANCH].checkout(True)
         try:
-            repo.create_remote("origin", config.UPSTREAM_REPO)
+            repo.create_remote("origin", UPSTREAM_REPO)
         except BaseException:
             pass
         nrs = repo.remote("origin")
