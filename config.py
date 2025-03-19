47e0245b2681f0dfc89f65fa996a960720b09d17
J9VX <dhruvraj500@gmail.com>
2025-03-20 00:14:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import re
 from os import getenv
-
 from dotenv import load_dotenv
 from pyrogram import filters
 
@@ -8,23 +7,16 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
-
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
-
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 6000))
-
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
-
 OWNER_ID = int(getenv("OWNER_ID", "6257927828"))
-
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VX-Opus/Opusv2",
+    "https://github.com/J9VX/VX7",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -32,18 +24,13 @@ GIT_TOKEN = getenv(
 )
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
-
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2d3fd5ccdd3d43dda6f17864d8eb7281")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "48d311d8910a4531ae81205e1f754d27")
-
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 50))
-
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
-
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/storm_techh")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/storm_core")
-
 START_IMG_URL = getenv("START_IMG_URL", "https://envs.sh/Oku.jpg")
 PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/9077cd2ba5818efef2d28.jpg")
 PLAYLIST_IMG_URL = getenv("PLAYLIST_IMG_URL", "https://graph.org/file/eb1e2b58e17964083db73.jpg")
