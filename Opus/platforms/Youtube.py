a4cf800dc0e9db3470867e926f2da049becba661
J9VX <dhruvraj500@gmail.com>
2025-03-26 00:25:19 +0530

    Update Youtube.py

--- Opus/platforms/Youtube.py
+++ Opus/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from Opus.utils.database import is_on_off
-from Opus.utils.formatters import time_to_seconds
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 
 
