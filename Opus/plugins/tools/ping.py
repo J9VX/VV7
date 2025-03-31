6d736d5d600cd894295865691122bd2ff79e65df
J9VX <dhruvraj500@gmail.com>
2025-03-31 20:32:10 +0530

    Update ping.py

--- Opus/plugins/tools/ping.py
+++ Opus/plugins/tools/ping.py
@@ -21,7 +21,7 @@ async def ping_com(client, message: Message, _):
     )
     pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 100
+    resp = (datetime.now() - start).microseconds / 10000
     await response.edit_text(
         _["ping_2"].format(resp, app.mention, UP),
         reply_markup=supp_markup(_),
