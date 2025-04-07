a7bc9b3887dde2f67a19460913b762b139dd12bb
J9VX <dhruvraj500@gmail.com>
2025-04-07 19:55:04 +0530

    Update play.py

--- Opus/plugins/play/play.py
+++ Opus/plugins/play/play.py
@@ -24,7 +24,7 @@ from Opus.utils.logger import play_logs
 from Opus.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
-sticker_id = "CAACAgUAAxkBAAKOGmfphS4-K6RkmUfZ2xvKBAys7AaTAAKiFwACWU9IV1Z0ZvLWZruuHgQ"
+sticker_id = "CAACAgUAAxkBAAKO-2fz39_HT38GB1-pPXHNs-0OsaoCAAIUFgAC7cWhVzXFEazHbHzRHgQ"
 
 @app.on_message(
     filters.command(
