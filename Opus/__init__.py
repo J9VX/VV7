6d50df6441d55a20d08b52c7d2a64dd5d410b982
J9VX <dhruvraj500@gmail.com>
2025-04-17 18:38:56 +0530

    Update __init__.py

--- Opus/__init__.py
+++ Opus/__init__.py
@@ -3,17 +3,17 @@ from Opus.core.dir import dirr
 from Opus.core.git import git
 from Opus.core.userbot import Userbot
 from Opus.misc import dbb, heroku
-import asyncio
 
 from .logging import LOGGER
 
 dirr()
-asyncio.run(git())
+git()
 dbb()
 heroku()
 
 app = Anony()
 userbot = Userbot()
+
 
 from .platforms import *
 
